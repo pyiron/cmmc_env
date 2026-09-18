e2318a53d6c9bb70860025895835142dd206e561
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2026-09-18 08:51:24 +0200

    Print output dictionary in dryrun.py
    Add print statement to output the dictionary contents.

--- .ci_support/dryrun.py
+++ .ci_support/dryrun.py
@@ -13,6 +13,8 @@ def get_detailed_environment(environment_input_file, environment_output_file):
     output_start_dict = json.loads(output_start)
     output_dict = output_start_dict.copy()
 
+    print(output_dict)
+
     if output_dict["name"] is None:
         del output_dict["name"]
     output_dict["dependencies"] = list(sorted([
